cdbef7230eb22268403a699579297560004eb6bd
Deividas <deividas.dambra@gmail.com>
2024-08-24 15:27:28 +0300

    Add final features and complete the project
    Added logic to check if the user inputs a number that is less than or
    equal to 100. Also moved the program's logic into the button's event
    listener

--- scripts.js
+++ scripts.js
@@ -1,11 +1,33 @@
-let squaresPerSide = prompt("Please enter the number of squares per side you would like to see in the grid:", "16");
+let squaresPerSide = 0;
+const button = document.querySelector(".btn");
 
-let totalSquares = squaresPerSide * squaresPerSide;
-const container = document.querySelector(".container");
-container.setAttribute("style", `display: flex; flex-wrap: wrap; width: ${squaresPerSide * 58}px`)
+button.addEventListener("click", () => {
+    while (true) {
+        squaresPerSide = prompt("Please enter the number of squares per side you would like to see in the grid (100 max):", "16");
 
-for (let i = 0; i  < totalSquares; i++) {
-    const square = document.createElement("div");
-    square.setAttribute("style", "width: 50px; height: 50px; margin: 2px; border: 2px solid black;")
-    container.appendChild(square);
-}
+        if (squaresPerSide <= 100) {
+            break;
+        }
+        alert("Please enter a number that is less than or equal to 100.");
+    }
+
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+
+    let totalSquares = squaresPerSide * squaresPerSide;
+
+    container.setAttribute("style", `display: flex; flex-wrap: wrap; width: ${squaresPerSide * 58}px`)
+
+    if (squaresPerSide <= 100) {
+        for (let i = 0; i < totalSquares; i++) {
+            const square = document.createElement("div");
+            square.setAttribute("style", "width: 50px; height: 50px; margin: 2px; border: 2px solid black;")
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "yellow";
+            })
+            container.appendChild(square);
+        }
+    } else {
+        squaresPerSide = prompt("Please enter the number of squares per side you would like to see in the grid:", "16");
+    }
+})
